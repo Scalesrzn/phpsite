db0d136876420670a5135398322d07dad330e358
Scales <Scalesrzn@gmail.com>
2019-12-21 16:11:22 +0300

    lab 3 push

--- lab_rab3.php
+++ lab_rab3.php
@@ -1,43 +1,36 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="style.css" rel="stylesheet">
-<link href="lab1.css" rel="stylesheet">
-<title>Сайт товарного склада "СкладовщикЪ"</title>
-<script src="//code.jivosite.com/widget.js" data-jv-id="U6hkUZJIfZ" async></script>
-</head>
-
-<body>
-
-<div class='main'>
-	
-	<div class="menu">
-		<?php include "menu.inc.php"?>
-	</div>
-	
-	<div class='header'>
-		<?php include "top.inc.php"?>
-	</div>
-
-	<div class="wrapper">
-		<div class='lborder'>
-		</div>
+<?php
+	session_start();
+	$from = 1;
+	$to = 10;
+	$report = '';
+	$story = array();
+	if (isset($_POST['submit'])) {
+		if (isset($_SESSION['count']) <=3)
+			$_SESSION['count']++;
+			$number = (int) $_POST['number'];
+			$story = array_push($story, isset($_POST['submit']) );
+			echo "<span> Вы вводили:  $story</span>";
+			if ($number == $_SESSION['number']) {
+				$report = 'Угадал с ' . $_SESSION['count'] . ' попыток';
+			}
+			else {
+				echo "</br>Не угадал!";
+			}
 		
-		<div class='center'>
-		</div>
-		<div class="rborder">
-		</div>
-	</div>
-	
-
-	<div class="footer">
-		<?php include "bottom.inc.php"?>
-	</div>
-</div>
-
-
-
-
-</body>
-</html>
+	} else {
+		$_SESSION['number'] = mt_rand($from, $to);
+		$_SESSION['count'] = 0;
+		
+	}
+ 
+?>
+        <div><?=$report ?></div>
+        <form action="<?php $_SERVER['SCRIPT_NAME']?>" method="POST">
+            <fieldset>
+                <legend>Введите число от <?=$from ?> до <?=$to ?></legend>
+                <label><p>
+                <input type="text" name="number" /></p></label>
+                <p><input class ='btn' type="submit" name="submit"  value="проверить"/></p>
+                <p><input class ='btn' type="submit" name="reset" value="начать заново" /></p>
+            </fieldset>
+        </form>
