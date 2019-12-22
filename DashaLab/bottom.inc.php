de72067d421a66c71c7fda1a2fdbc216151e0906
Scales <Scalesrzn@gmail.com>
2019-12-22 16:11:31 +0300

    Lab3 Completed

--- DashaLab/bottom.inc.php
+++
@@ -1,6 +0,0 @@
-<?php date_default_timezone_set('Europe/Moscow')?>
-<p class="bottom">
-	Ваш последний визит: <?=$dateVisit?>
-	<br/> Текущая дата и время: <?=date("d.m.Y H:i")?>
-	<br/> Powered by <?=$_SERVER['SERVER_SOFTWARE']?>
-</p>  
